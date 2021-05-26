ae73c7ad35df58306458fd60abbd1501305aa3c8
buba71 <davdelima71@gmail.com>
2021-05-26 20:41:10 +0200

    build: update assets && add webpack encore

---
+++ src/Presentation/Web/Controller/SecurityController.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Presentation\Web\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+
+final class SecurityController
+{
+    public function login(Environment $environment): Response
+    {
+        return new Response($environment->render('Security/login.html.twig'));
+    }
+
+}
